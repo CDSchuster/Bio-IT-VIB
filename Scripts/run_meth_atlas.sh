e485bff2cc7aa5a1d63f52c114d853a0a3b5a0e1
CDSchuster <claudio.schuster.93@gmail.com>
2025-05-06 16:51:57 -0300

    Modified repo structure and added an Rscript for differential methylation analysis

---
+++ Scripts/run_meth_atlas.sh
@@ -0,0 +1,1 @@
+python3 ~/Projects/meth_atlas/deconvolve.py ~/Projects/Bio-IT-VIB/Data/bulk_samples.csv --atlas_path ~/Projects/Bio-IT-VIB/Data/reference_samples.csv --out_dir ~/Projects/Bio-IT-VIB/Outputs
--- run_meth_atlas.sh
+++
@@ -1,1 +0,0 @@
-python3 ~/Projects/meth_atlas/deconvolve.py bulk_samples.csv --atlas_path reference_samples.csv
